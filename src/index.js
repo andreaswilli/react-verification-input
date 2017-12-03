8eae2ded623ee1d3fbee8bfc18d16bb8d7af5fcf
Andreas Willi <andiwilli8460@gmail.com>
2017-12-03 01:06:50 +0100

    Setup styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react';
 
+import './styles.scss';
+
 export default class VerificationInput extends PureComponent {
   render () {
     return (
-      <div>Verification input :)</div>
+      <div className="purple semi-bold">Verification input :)</div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,17 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' }
     ]
+  },
+  externals: {
+    'react': 'commonjs react'
   }
 };
