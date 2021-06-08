5f939399ec0d09c0274927e5ccea4b2303c1f816
Andreas Willi <andiwilli8460@gmail.com>
2021-06-08 13:36:21 +0200

    Replace npmignore with files array
    Include files to be packaged instead of excluding them in npmignore. Also, rename dist folder to lib.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "lib"),
     filename: "index.js",
     libraryTarget: "commonjs2",
   },
