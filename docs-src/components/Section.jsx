e8127edbde34f9f0d9cb50a2ddce814bc856316c
Andreas Willi <andiwilli8460@gmail.com>
2022-08-16 00:57:21 +0200

    Add docs and demo website

---
+++ docs-src/components/Section.jsx
@@ -0,0 +1,14 @@
+export default function Section({ markdown, type, name, removeHeading = false }) {
+  const md = markdown()
+  let fromIndex = md.props.children.findIndex(c => c.props?.children === name)
+  let toIndex = fromIndex + 1 + md.props.children.slice(fromIndex + 1).findIndex(c => c.type === type)
+
+  if (removeHeading) {
+    fromIndex += 1
+  }
+
+  return <>
+    {md.props.children.slice(fromIndex, toIndex)}
+  </>
+}
+
---
+++ docs-src/pages/_app.js
@@ -0,0 +1,6 @@
+import "nextra-theme-docs/style.css";
+import "./styles.css";
+
+export default function Nextra({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
